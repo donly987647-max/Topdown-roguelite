baba20db81b2e2cbc04c25486aaa9199316bd8a6
Doong <donly987647@gmail.com>
2026-07-29 00:29:14 +0900

    Add offline cache for complete web build

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='last-magazine-1.0.1';
+const ASSETS=['./','./index.html','./style.css','./data.js','./game-1.js','./game-2-1.js','./game-2-2.js','./game-2-3.js','./game-2-4.js','./game-3-1.js','./game-3-2.js','./game-3-3.js','./game-4-1.js','./game-4-2.js','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html')))));
