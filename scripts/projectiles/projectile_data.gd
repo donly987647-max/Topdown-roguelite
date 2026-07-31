7d9603892adf7b05d0fb461c17c6cce7dd4109aa
Doong <donly987647@gmail.com>
2026-07-31 15:09:34 +0900

    Extend projectile data for P2 weapon parts

--- scripts/projectiles/projectile_data.gd
+++ scripts/projectiles/projectile_data.gd
@@ -6,10 +6,13 @@ extends Resource
 @export var lifetime := 1.5
 @export var radius := 3.0
 @export var pierce_count := 0
+@export var pierce_damage_decay := 0.0
 @export var ricochet_count := 0
+@export var ricochet_damage_multiplier := 1.0
 @export var homing_strength := 0.0
 @export var knockback := 35.0
 @export var critical_chance := 0.05
+@export var status_type: StringName = &""
 @export var status_buildup := 0.0
 @export var explosion_radius := 0.0
 @export var faction: StringName = &"player"
