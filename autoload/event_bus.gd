dc7b1c5d2785056be82daaca5988105270ed25f6
Doong <donly987647@gmail.com>
2026-07-31 15:52:35 +0900

    Add backpack inventory request signal

--- autoload/event_bus.gd
+++ autoload/event_bus.gd
@@ -8,4 +8,5 @@ signal hit_landed(position: Vector2, strength: float, critical: bool)
 signal player_damaged(amount: float, source_position: Vector2)
 signal precision_dodge(position: Vector2)
 signal screen_shake(strength: float, source_position: Vector2)
+signal inventory_requested
 signal run_reset_requested
