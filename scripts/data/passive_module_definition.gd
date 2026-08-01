449b5b4a29ef89bea11b1c4e575d3f7cc2d1a870
Doong <donly987647@gmail.com>
2026-08-02 07:53:55 +0900

    Add passive module definition

---
+++ scripts/data/passive_module_definition.gd
@@ -0,0 +1,6 @@
+class_name PassiveModuleDefinition
+extends BackpackItemDefinition
+
+@export var stat_modifiers: Dictionary = {}
+@export var trigger_effect_ids: PackedStringArray = []
+@export var max_stacks: int = 1
