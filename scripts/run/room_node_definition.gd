d0689ad2b0241878d53a601f6bdba631929f9308
Doong <donly987647@gmail.com>
2026-08-02 07:54:57 +0900

    Add room node definition

---
+++ scripts/run/room_node_definition.gd
@@ -0,0 +1,8 @@
+class_name RoomNodeDefinition
+extends Resource
+
+@export var id: StringName
+@export var room_type: StringName = &"combat"
+@export var difficulty: int = 1
+@export var reward_tags: PackedStringArray = []
+@export var metadata: Dictionary = {}
