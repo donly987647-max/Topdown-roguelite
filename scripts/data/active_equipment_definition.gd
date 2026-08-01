f356c97e5b7b186b4ead16adb8a3b067185c4056
Doong <donly987647@gmail.com>
2026-08-02 07:54:01 +0900

    Add active equipment definition

---
+++ scripts/data/active_equipment_definition.gd
@@ -0,0 +1,7 @@
+class_name ActiveEquipmentDefinition
+extends BackpackItemDefinition
+
+@export var cooldown: float = 8.0
+@export var charges: int = 1
+@export var effect_ids: PackedStringArray = []
+@export var activation_payload: Dictionary = {}
