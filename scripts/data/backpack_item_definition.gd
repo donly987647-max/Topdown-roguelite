3b6766574db73921d2064c261cfea6d2e6320bb5
Doong <donly987647@gmail.com>
2026-08-02 02:22:50 +0900

    Add backpack item definition resource

---
+++ scripts/data/backpack_item_definition.gd
@@ -0,0 +1,14 @@
+class_name BackpackItemDefinition
+extends Resource
+
+@export var id: StringName
+@export var display_name: String
+@export_multiline var description: String
+@export var rarity: StringName = &"common"
+@export var cells: PackedVector2Array = [Vector2(0, 0)]
+@export var rotatable: bool = true
+@export var connector_types: Dictionary = {}
+@export var tags: PackedStringArray = []
+@export var adjacency_effect_ids: PackedStringArray = []
+@export var effect_ids: PackedStringArray = []
+@export var power_draw: float = 0.0
